fa6ca0692ddebe70c26a030fbfad16cc54a60a2c
ahmad1091 <ahmadtayeb91@gmail.com>
2022-08-19 19:02:36 +0300

    add the Articale model

---
+++ blog_app/blog/models.py
@@ -0,0 +1,37 @@
+from email.policy import default
+import os
+import sqlite3
+from turtle import title
+import _sqlite3
+import uuid
+from typing import List
+
+from pydantic import BaseModel, EmailStr, Field
+
+class NotFound(Exception):
+    pass
+
+
+class Article(BaseModel):
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    author: EmailStr
+    title: str
+    content: str
+
+    @classmethod
+    def get_by_id(cls, article_id: str):
+        con = sqlite3.connect(os.getenv("DATABASE_NAME", "database.db"))
+        con.row_factory = sqlite3.Row
+
+        cur = con.cursor()
+        cur.execute("SELECT * FROM articles WHERE id=?", (article_id,))
+
+        record = cur.fetchone()
+
+        if record is None:
+            raise NotFound
+
+        article = cls(**record)  # Row can be unpacked as dict
+        con.close()
+
+        return article
