85a3c458df3f186a5efa3ab9cc2887d50d8da72c
ahmad1091 <ahmadtayeb91@gmail.com>
2022-08-19 21:13:17 +0300

    add the acutal logic for GetArticleByIDQuery

--- blog_app/blog/queries.py
+++ blog_app/blog/queries.py
@@ -10,3 +10,12 @@ class ListArticlesQuery(BaseModel):
     def execute(self) -> List[Article]:
         articles = Article.list()
         return articles
+
+
+class GetArticleByIDQuery(BaseModel):
+    id: str
+
+    def execute(self) -> Article:
+        article = Article.get_by_id(self.id)
+
+        return article
