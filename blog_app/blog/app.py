7b03399978775c041714f92c9db2e3802b4d21cb
ahmad1091 <ahmadtayeb91@gmail.com>
2022-08-20 10:00:15 +0300

    add the create-artcile route

--- blog_app/blog/app.py
+++ blog_app/blog/app.py
@@ -5,3 +5,9 @@ from blog.queries import GetArticleByIDQuery, ListArticlesQuery
 
 app = Flask(__name__)
 
+@app.route("/create-article/", methods=["POST"])
+def create_article():
+    cmd = CreateArticleCommand(
+        **request.json
+    )
+    return jsonify(cmd.execute().dict())
